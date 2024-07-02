2c7d115ad88318561c97fde55ba85ea9b1c1ef02
Mark Nguyen <mark_nguyen@berkeley.edu>
2024-07-01 20:37:25 -0700

    game card component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,15 +1,20 @@
 import useGames from "../hooks/useGames";
-
+import GameCard from "./GameCard";
+import { SimpleGrid } from "@chakra-ui/react";
 const GameGrid = () => {
   const { games, error } = useGames();
 
   if (error) return error;
   return (
-    <ul>
+    <SimpleGrid
+      spacing={10}
+      columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+      padding={10}
+    >
       {games.map((game) => (
-        <li key={game.id}>{game.name}</li>
+        <GameCard game={game} key={game.id}></GameCard>
       ))}
-    </ul>
+    </SimpleGrid>
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
-interface Game {
+
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 interface GameResponse {
   count: number;
@@ -21,7 +23,7 @@ const useGames = () => {
       .get<GameResponse>("/games", { signal: controller.signal })
       .then((res) => {
         setGames(res.data.results);
-        console.log(res.data.results);
+        console.log("game ressult: " + res.data.results);
       })
       .catch((err) => {
         if (err instanceof CanceledError) return;
@@ -30,7 +32,6 @@ const useGames = () => {
 
     return () => controller.abort();
   }, []);
-
   return { games, error };
 };
 
